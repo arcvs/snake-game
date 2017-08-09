6806b980ad5e345886d4c88639c20fe2afbeed4b
Vladimir <arctic.vs@gmail.com>
2017-08-09 12:15:33 +0300

    fix

--- game_n.js
+++ game_n.js
@@ -6,57 +6,67 @@ var RIGHT   = 39;
 var DOWN    = 40;
 var PAUSE   = 32;
 
-function pip(x,y){
-  return x + y;
-}
+var domBlockId = $('#gameZone');
+var statsBlockId = $('#stats');
+
 
 function Field(cols, rows) {
 	this.cols = cols;
 	this.rows = rows;
 	this.numberSectors = this.cols * this.rows;
 
-	this.hunger = true;
-  this.numberSectorMeat;
+	this.hunger = false;
+  this.positionMeat;
   this.cells;
   this.generationMap();
 };
 
 Field.prototype.generationMap = function() {
-	for (var i = 0; i < this.numberSectors; i++) 
-		$('#gameZone').append('<div>');
+	for (var i = 0; i < this.numberSectors; i++) {
+    domBlockId.append('<div>');
+  }
 
-  $('#gameZone').css('width', this.cols * 15 + this.cols);
+  domBlockId.css('width', this.cols * 15 + this.cols);
 
-  this.cells = $("#gameZone").children();
+  this.cells = domBlockId.children();
 };
 
-Field.prototype.generationMeat = function() {
-	this.hunger = false;
+Field.prototype.generationMeat = function(python) {
+
+  var self = this;
+
 	function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	};
-	this.numberSectorMeat = getRandomInt(0, this.numberSectors - 1);
-	this.cells[this.numberSectorMeat].className = 'pig';
+  }
+
+  (function checkPositionPythonAndMeat() {
+    self.positionMeat = getRandomInt(0, self.numberSectors - 1);
+    python.forEach(function(item, i){
+      if (item === self.positionMeat) {
+        return checkPositionPythonAndMeat();
+      }
+    })
+  })()
+
+  this.hunger = false;
+	this.cells[this.positionMeat].className = 'meat';
 };
 
 function converterPositionToAxes(field, a) {
   var x = a % field.cols;
   var y = Math.floor(a / field.cols);
-  //console.log({x: x, y: y});
   return {x: x, y: y};
-}
+};
 
 function converterAxesToPosition(field, a, b) {
   var position = b * field.cols + a;
-  //console.log(position);
   return position;
-}
+};
 
 function Python(game, field) {
-
 	this.direction = {x : 0, y : 0};
   this.body = this.setStartPositionFild(field);
-  this.nextStepAxes;
+  this.nextPositionStep = NaN;
 
   this._step = function(i) {
 
@@ -68,29 +78,31 @@ function Python(game, field) {
       head.y += this.direction.y;
       this.throughWallsOn(head);
       this.body[i] = converterAxesToPosition(field, head.x, head.y);
+
       this.deadLoop(this.body[i]);
       this.eatsMeat(this.body[i]);
     } else {
-      this.body[i] = this.nextStepAxes || backStep;
+      // NaN - обеспечивает правильную работу части тела в нулевой позиции 
+      this.body[i] = isNaN(this.nextPositionStep) ? backStep : this.nextPositionStep;
     }
 
     if (game.gameover) { 
       return 0; 
     }
 
-    field.cells[this.body[i]].className = 'active';
+    field.cells[this.body[i]].className = 'active' + (!i?0:'');
 
     if (this.direction.x || this.direction.y) {
       field.cells[backStep].className = '';
       return backStep;
     }
   }
-}
+};
 
 Python.prototype.render = function() {
   var self = this;
   this.body.forEach(function(iteam, i) {
-    self.nextStepAxes = self._step(i);
+    self.nextPositionStep = self._step(i);
   });
 }
 
@@ -103,8 +115,8 @@ Python.prototype.throughWallsOn = function(head) {
 
 Python.prototype.setStartPositionFild = function(field) {
   var position = field.cols * field.rows / 2 - field.cols / 2;
-  return [position, position-1, position-2, position-3, position-4, position-5];
-}
+  return [position, position-1];
+};
 
 Python.prototype.deadLoop = function(positionHead) {
   this.body.forEach(function(posPart, i){
@@ -113,15 +125,13 @@ Python.prototype.deadLoop = function(positionHead) {
       field.cells[posPart].style.background = 'red';
     }
   });
-}
+};
 
 Python.prototype.eatsMeat = function(positionHead) {
-  if (positionHead === field.numberSectorMeat) {
-    console.log('eat');
-    this.body.push(new Pixel(bodyTailX, bodyTailY));
-    this.field.hunger = true;
-    field.cells[this.field.numberSectorMeat].className = '';
-    this.speed -= 100;
+  if (positionHead === field.positionMeat) {
+    this.body.push(0);
+    field.hunger = true;
+    field.cells[field.positionMeat].className = '';
   }
 };
 
@@ -135,20 +145,25 @@ function Game(){
 
 Game.prototype.run = function(field, python) {
 
-  python.render();
-
   var self = this;
 
-	var timerIteration = setInterval( function() {
+  python.render();
+  statsBlockId.text('Speed: ' + this.speed + 'ms');
 
-    if ( field.hunger ) field.generationMeat(python.body);
-
-		if(self.active && !self.gameover) { 
-      python.render();
-    } else {
-      //clearInterval( timerIteration ); 
-		}
-	}, self.speed);
+  (function setTime(){
+    field.generationMeat(python.body);
+    var timerIteration = setInterval( function() {
+      if (field.hunger) {
+        self.speed -= 5;
+        statsBlockId.text('Speed: ' + self.speed + 'ms');
+        clearInterval( timerIteration );
+        setTime();
+      }
+      if(self.active && !self.gameover) { 
+        python.render();
+      }
+    }, self.speed);
+  })()
 };
 
 Game.prototype.start = function() {
@@ -160,7 +175,7 @@ Game.prototype.pause = function() {
 
 var game = new Game();
 
-var field = new Field(14, 10); //field.generationMap();
+var field = new Field(28, 20);
 var python = new Python(game, field);
 
 game.run(field, python);
@@ -180,76 +195,4 @@ document.onkeydown = function(event) {
 	if((event.keyCode >= 32) && (event.keyCode <= 40)) {
     dispatcherEventKey(event.keyCode);
 	};
-};
-
-
-
-
-
-
-
-
-
-function Snake( field ) {
-
-	var self = this;
-
-	this.numberSectorHead;
-
-  this.visualization = function() {
-		this.body.forEach( function(iteam, i) {
-			//if ( self.activity ) self._tempPosition = self._render( i );
-    });
-  };
-	
-	var fieldDOM = $("#gameZone").children();
-	
-	this._render = function(part) {
-
-		if (!this.directionHead.x && !this.directionHead.y) {
-			fieldDOM[ this.body[part].y  + this.body[part].x ].className = 'active';
-			return false;
-		}
-
-		var lengthSnake = this.body.length - 1;
-
-		var tempX = this.body[part].x;
-		var tempY = this.body[part].y;
-
-    if ( part == 0 ) {
-
-			this.body[0].x += this.directionHead.x;
-			this.body[0].y += this.directionHead.y * this.field.cols;
-
-			this.throughFaces();
-			this.numberSectorHead = this.body[0].x + this.body[0].y;
-			this.loop();
-
-			this.eatsMeat( this.body[lengthSnake].x, this.body[lengthSnake].y );
-
-			//if ( !this.activity ) return;
-		} else {
-			this.body[part].x = this._tempPosition.x;
-			this.body[part].y = this._tempPosition.y;
-		}
-
-		fieldDOM[ this.body[part].y  + this.body[part].x ].className = 'active';
-		fieldDOM[ tempY + tempX ].className = '';
-		
-		return {x: tempX, y: tempY};
-	};
-}; 
-
-Snake.prototype.eatsMeat = function(bodyTailX, bodyTailY) {
-	if (this.numberSectorHead == this.field.numberSectorMeat) {
-		this.body.push( new Pixel( bodyTailX, bodyTailY ) );
-		this.field.hunger = true;
-		$("#gameZone").children()[this.field.numberSectorMeat].className = '';
-		this.speed -= 100;
-	}
-};
-
-Snake.prototype.sleep = function() {
-	//this.pauseSnake = ( this.pauseSnake ) ? false : this.directionHead;
-	this.directionHead = {x: 0, y: 0};
 };
